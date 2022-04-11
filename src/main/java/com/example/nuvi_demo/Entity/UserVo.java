c48157fe3dc5b29c3cb5631b517ab05cb760e1d9
Gyeo1 <rlatjdruf99@naver.com>
2022-04-11 17:35:24 +0900

    Entity(DB에 쓰일 필드)와 VO(객체 받는용도) 생성

---
+++ src/main/java/com/example/nuvi_demo/Entity/UserVo.java
@@ -0,0 +1,23 @@
+package com.example.nuvi_demo.Entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//UserVo는 로그인 할때 id/password를 받기위한 객체!
+
+@Data // user 필드값의 getter를 자동으로 생성합니다.
+@NoArgsConstructor // 인자없는 생성자를 자동으로 생성합니다.
+@AllArgsConstructor // 인자를 모두 갖춘 생성자를 자동으로 생성합니다.
+public class UserVo {
+
+    @JsonProperty
+    private String id;
+
+    @JsonProperty
+    private String password;
+
+
+
+}
